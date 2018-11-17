fed15147789a208190ff1dd15dbd07430711dee2
nobodyzxc <dici86.chen@gmail.com>
2018-11-18 04:07:40 +0800

    first commit

---
+++ js/bank.js
@@ -0,0 +1,298 @@
+let bankBytecode = "6080604052336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550610f7c806100536000396000f300608060405260043610610099576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680632e1a7d4d1461009e57806341c0e1b5146100cb5780637b83b50b146100e2578063a9059cbb1461010d578063aa3e70d21461015a578063d0e30db014610171578063d750f81c1461017b578063d971b65e146101ad578063ffbf5900146101cd575b600080fd5b3480156100aa57600080fd5b506100c9600480360381019080803590602001909291905050506101fa565b005b3480156100d757600080fd5b506100e06103ae565b005b3480156100ee57600080fd5b506100f76104ac565b6040518082815260200191505060405180910390f35b34801561011957600080fd5b50610158600480360381019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190803590602001909291905050506104f3565b005b34801561016657600080fd5b5061016f6106c5565b005b610179610916565b005b34801561018757600080fd5b506101906109bb565b604051808381526020018281526020019250505060405180910390f35b6101cb60048036038101908080359060200190929190505050610a4c565b005b3480156101d957600080fd5b506101f860048036038101908080359060200190929190505050610c70565b005b6000670de0b6b3a76400008202905080600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002054101515156102c0576040517f08c379a000000000000000000000000000000000000000000000000000000000815260040180806020018281038252601c8152602001807f796f75722062616c616e63657320617265206e6f7420656e6f7567680000000081525060200191505060405180910390fd5b3373ffffffffffffffffffffffffffffffffffffffff166108fc829081150290604051600060405180830381858888f19350505050158015610306573d6000803e3d6000fd5b5080600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600082825403925050819055503373ffffffffffffffffffffffffffffffffffffffff167f5bb95829671915ece371da722f91d5371159095dcabf2f75cd6c53facb7e1bab8342604051808381526020018281526020019250505060405180910390a25050565b3373ffffffffffffffffffffffffffffffffffffffff166000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16141515610472576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260118152602001807f796f7520617265206e6f74206f776e657200000000000000000000000000000081525060200191505060405180910390fd5b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16ff5b6000600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002054905090565b6000670de0b6b3a76400008202905080600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002054101515156105b9576040517f08c379a000000000000000000000000000000000000000000000000000000000815260040180806020018281038252601c8152602001807f796f75722062616c616e63657320617265206e6f7420656e6f7567680000000081525060200191505060405180910390fd5b80600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000206000828254039250508190555080600160008573ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600082825401925050819055508273ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff167fbabc8cd3bd6701ee99131f374fd2ab4ea66f48dc4e4182ed78fecb0502e44dd68442604051808381526020018281526020019250505060405180910390a3505050565b600080600080600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000206000015414151515610784576040517f08c379a000000000000000000000000000000000000000000000000000000000815260040180806020018281038252601c8152602001807f6365727469666963617465206465706f736974206e6f2065786973740000000081525060200191505060405180910390fd5b600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600001549250600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000206001015491506108188383610f35565b9050600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600080820160009055600182016000905550503373ffffffffffffffffffffffffffffffffffffffff166108fc849081150290604051600060405180830381858888f193505050501580156108b2573d6000803e3d6000fd5b503373ffffffffffffffffffffffffffffffffffffffff167fe44ee01a5ab07517841c736941342536e8b1f18b9a132be3e6989702caf7379182844260405180848152602001838152602001828152602001935050505060405180910390a2505050565b34600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600082825401925050819055503373ffffffffffffffffffffffffffffffffffffffff167fad40ae5dc69974ba932d08b0a608e89109412d41d04850f5196f144875ae26603442604051808381526020018281526020019250505060405180910390a2565b600080600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060000154600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060010154915091509091565b6000600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060000154141515610b2c576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260248152602001807f70726576696f7573206365727469666963617465206465706f7369742065786981526020017f737465640000000000000000000000000000000000000000000000000000000081525060400191505060405180910390fd5b600081111515610ba4576040517f08c379a000000000000000000000000000000000000000000000000000000000815260040180806020018281038252601b8152602001807f706572696f6473206d757374206d6f7265207468616e207a65726f000000000081525060200191505060405180910390fd5b604080519081016040528034815260200182815250600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060008201518160000155602082015181600101559050503373ffffffffffffffffffffffffffffffffffffffff167ff7293c5e8d3095accc67b5d38b9f2ce7c590369570a8f3fbc86a8f11f763020d34834260405180848152602001838152602001828152602001935050505060405180910390a250565b6000806000600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000206000015414151515610d2e576040517f08c379a000000000000000000000000000000000000000000000000000000000815260040180806020018281038252601c8152602001807f6365727469666963617465206465706f736974206e6f2065786973740000000081525060200191505060405180910390fd5b600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600101548311151515610de8576040517f08c379a000000000000000000000000000000000000000000000000000000000815260040180806020018281038252601e8152602001807f706572696f6473206f766572206465706f736974277320706572696f6473000081525060200191505060405180910390fd5b600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600001549150610e378284610f35565b9050600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600080820160009055600182016000905550503373ffffffffffffffffffffffffffffffffffffffff166108fc839081150290604051600060405180830381858888f19350505050158015610ed1573d6000803e3d6000fd5b503373ffffffffffffffffffffffffffffffffffffffff167f652eabdb7b5551bd3e0987861a2e7467637509eff9277da096840518b4525d5382854260405180848152602001838152602001828152602001935050505060405180910390a2505050565b60006064828402811515610f4557fe5b0483019050929150505600a165627a7a7230582078787def52a8a9f144b16fbd5fa5f218cd71193fc3899ec4d3a8288600ac74f20029";
+
+let bankAbi = [
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "etherValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdraw",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "kill",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getBankBalance",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "name": "etherValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "completeCD",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "deposit",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getCD",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "periods",
+        "type": "uint256"
+      }
+    ],
+    "name": "purchaseCD",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "periods",
+        "type": "uint256"
+      }
+    ],
+    "name": "terminateCD",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "name": "DepositEvent",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "name": "WithdrawEvent",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "name": "TransferEvent",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "periods",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "name": "PurchaseCDEvent",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "periods",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "name": "TerminateCDEvent",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "periods",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "name": "CompleteCDEvent",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "name": "DuplicateCD",
+    "type": "event"
+  }
+];
---
+++ js/index.js
@@ -0,0 +1,537 @@
+"use strict";
+
+let contractAddress = $("#contractAddress");
+let deployedContractAddressInput = $("#deployedContractAddressInput");
+let loadDeployedContractButton = $("#loadDeployedContractButton");
+let deployNewContractButton = $("#deployNewContractButton");
+
+let killContractButton = $("#killContractButton");
+
+let whoami = $("#whoami");
+let whoamiButton = $("#whoamiButton");
+let copyButton = $("#copyButton");
+
+let update = $("#update");
+
+let logger = $("#logger");
+
+let deposit = $("#deposit");
+let depositButton = $("#depositButton");
+
+let withdraw = $("#withdraw");
+let withdrawButton = $("#withdrawButton");
+
+let transferEtherTo = $("#transferEtherTo");
+let transferEtherValue = $("#transferEtherValue");
+let transferEtherButton = $("#transferEtherButton");
+
+let purchaseCDValue = $("#purchaseCDValue" );
+let purchaseCDPeriods = $("#purchaseCDPeriods");
+let purchaseCDButton = $("#purchaseCDButton");
+
+let completeCDButton = $("#completeCDButton");
+let terminateCDPeriods = $("#terminateCDPeriods");
+let terminateCDButton = $("#terminateCDButton");
+
+let bankAddress = "";
+let nowAccount = "";
+
+// let web3 = new Web3('http://localhost:8545');
+let web3 = new Web3(
+  new Web3.providers.WebsocketProvider("ws://localhost:8545")
+);
+
+let bank = new web3.eth.Contract(bankAbi);
+
+
+function log(...inputs) {
+
+  inputs.reverse();
+  for (let input of inputs) {
+    if (typeof input === "object") {
+      input = JSON.stringify(input, null, 2);
+    }
+    else if(typeof input == "string") {
+      if(input.indexOf("成功") != -1){
+        alert(input);
+      }
+    }
+    //logger.html(input + "\n" + logger.html());
+  }
+  logger.html(inputs.map(function(x){ return JSON.stringify(x, null, 2); }).join("\n") + "\n" + logger.html());
+}
+
+function guard(name, val_string){
+  if ($.trim(val_string) == "") {
+    alert("請輸入" + name);
+    return true;
+  }
+  if (parseInt(val_string, 10) <= 0) {
+    alert(name + "必須大於零");
+    return true;
+  }
+  return false;
+}
+
+init();
+
+async function init() {
+  let accounts = await web3.eth.getAccounts();
+
+  for (let account of accounts) {
+    whoami.append(`<option value="${account}">${account}</option>`);
+  }
+  nowAccount = whoami.val();
+
+  update.trigger("click");
+
+  log(accounts, "以太帳戶");
+}
+
+// 當按下載入既有合約位址時
+loadDeployedContractButton.on("click", function() {
+  loadBank(deployedContractAddressInput.val());
+});
+
+// 當按下部署合約時
+deployNewContractButton.on("click", function() {
+  newBank();
+});
+
+// 當按下登入按鍵時
+whoamiButton.on("click", function() {
+  nowAccount = whoami.val();
+
+  update.trigger("click");
+});
+
+// 當按下複製按鍵時
+copyButton.on("click", function() {
+  let textarea = $("<textarea />");
+  textarea
+    .val(whoami.val())
+    .css({
+      width: "0px",
+      height: "0px",
+      border: "none",
+      visibility: "none"
+    })
+    .prependTo("body");
+
+  textarea.focus().select();
+
+  try {
+    if (document.execCommand("copy")) {
+      textarea.remove();
+      return true;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  textarea.remove();
+  return false;
+});
+
+// 當按下更新按鍵時
+update.on("click", async function() {
+  if (bankAddress != "") {
+    let ethBalance = await web3.eth.getBalance(nowAccount);
+    let bankBalance = await bank.methods
+      .getBankBalance()
+      .call({ from: nowAccount });
+    let cd = await bank.methods
+      .getCD()
+      .call({ from: nowAccount });
+
+    log({
+      address: bankAddress,
+      ethBalance: ethBalance,
+      bankBalance: bankBalance,
+      certificateDeposit: "(" + cd["0"] + ", " + cd["1"] + ")"
+    });
+    log("更新帳戶資料");
+
+    $("#ethBalance").text("以太帳戶餘額 (wei): " + ethBalance);
+    $("#bankBalance").text("銀行ETH餘額 (wei): " + bankBalance);
+    $("#certificateDeposit").text("定存 (wei, 期數): (" + cd["0"] + ", " + cd["1"] + ")");
+  } else {
+    let ethBalance = await web3.eth.getBalance(nowAccount);
+
+    $("#ethBalance").text("以太帳戶餘額 (wei): " + ethBalance);
+    $("#bankBalance").text("銀行ETH餘額 (wei): ");
+  }
+});
+
+// 當按下刪除合約按鈕時
+killContractButton.on("click", async function() {
+  if (bankAddress == "") {
+    return;
+  }
+
+  // 解鎖
+  let unlock = await unlockAccount();
+  if (!unlock) {
+    return;
+  }
+
+  // 更新介面
+  waitTransactionStatus();
+  // 刪除合約
+  bank.methods
+    .kill()
+    .send({
+      from: nowAccount,
+      gas: 3400000
+    })
+    .on("receipt", function(receipt) {
+      log(bankAddress, "成功刪除合約");
+
+      bankAddress = "";
+      contractAddress.text("合約位址:" + bankAddress);
+      deployedContractAddressInput.val(bankAddress);
+
+      // 觸發更新帳戶資料
+      update.trigger("click");
+
+      // 更新介面
+      doneTransactionStatus();
+    })
+    .on("error", function(error) {
+      log(error.toString());
+      // 更新介面
+      doneTransactionStatus();
+    });
+});
+
+// 當按下存款按鍵時
+depositButton.on("click", async function() {
+  if (bankAddress == "") {
+    return;
+  }
+  if (guard("存款金額", deposit.val())) {
+    return;
+  }
+
+  // 解鎖
+  let unlock = await unlockAccount();
+  if (!unlock) {
+    return;
+  }
+
+  // 更新介面
+  waitTransactionStatus();
+  // 存款
+  bank.methods
+    .deposit()
+    .send({
+      from: nowAccount,
+      gas: 3400000,
+      value: web3.utils.toWei(deposit.val(), "ether")
+    })
+    .on("receipt", function(receipt) {
+      log(receipt.events.DepositEvent.returnValues, "存款成功");
+
+      // 觸發更新帳戶資料
+      update.trigger("click");
+
+      // 更新介面
+      doneTransactionStatus();
+    })
+    .on("error", function(error) {
+      log(error.toString());
+      // 更新介面
+      doneTransactionStatus();
+    });
+  bank.events.DepositEvent().on("data", function(event) {
+    log(event, "DepositEvent");
+  });
+});
+
+// 當按下提款按鍵時
+withdrawButton.on("click", async function() {
+  if (bankAddress == "") {
+    return;
+  }
+  if (guard("提款金額", withdraw.val())) {
+    return;
+  }
+  // 解鎖
+  let unlock = await unlockAccount();
+  if (!unlock) {
+    return;
+  }
+
+  // 更新介面
+  waitTransactionStatus();
+  // 提款
+  bank.methods
+    .withdraw(parseInt(withdraw.val(), 10))
+    .send({
+      from: nowAccount,
+      gas: 3400000
+    })
+    .on("receipt", function(receipt) {
+      log(receipt.events.WithdrawEvent.returnValues, "提款成功");
+
+      // 觸發更新帳戶資料
+      update.trigger("click");
+
+      // 更新介面
+      doneTransactionStatus();
+    })
+    .on("error", function(error) {
+      alert(error.toString());
+      log("(錢不夠提?)" + error.toString());
+      // 更新介面
+      doneTransactionStatus();
+    });
+});
+
+// 當按下轉帳按鍵時
+transferEtherButton.on("click", async function() {
+  if (bankAddress == "") {
+    return;
+  }
+  if(guard("轉帳額度", transferEtherValue.val())) {
+    return;
+  }
+  if($.trim(transferEtherTo.val()) == "") {
+    alert("轉帳對象不能為空");
+    return;
+  }
+  // 解鎖
+  let unlock = await unlockAccount();
+  if (!unlock) {
+    return;
+  }
+
+  // 更新介面
+  waitTransactionStatus();
+  // 轉帳
+  bank.methods
+    .transfer(transferEtherTo.val(), parseInt(transferEtherValue.val(), 10))
+    .send({
+      from: nowAccount,
+      gas: 3400000
+    })
+    .on("receipt", function(receipt) {
+      log(receipt.events.TransferEvent.returnValues, "轉帳成功");
+
+      // 觸發更新帳戶資料
+      update.trigger("click");
+
+      // 更新介面
+      doneTransactionStatus();
+    })
+    .on("error", function(error) {
+      log(error.toString());
+      // 更新介面
+      doneTransactionStatus();
+    });
+});
+
+// 當按下購買定存按鍵時
+purchaseCDButton.on("click", async function() {
+  if (bankAddress == "") {
+    return;
+  }
+  if(guard("定存金額", purchaseCDValue.val())) {
+    return;
+  }
+  if(guard("定存期數", purchaseCDPeriods.val())) {
+    return;
+  }
+  // 解鎖
+  let unlock = await unlockAccount();
+  if (!unlock) {
+    return;
+  }
+
+  // 更新介面
+  waitTransactionStatus();
+  // 買定存
+  bank.methods
+    .purchaseCD(parseInt(purchaseCDPeriods.val(), 10))
+    .send({
+      from: nowAccount,
+      gas: 3400000,
+      value: web3.utils.toWei(purchaseCDValue.val(), "ether")
+    })
+    .on("receipt", function(receipt) {
+      log(receipt.events.PurchaseCDEvent.returnValues, "購買定存成功");
+
+      // 觸發更新帳戶資料
+      update.trigger("click");
+
+      // 更新介面
+      doneTransactionStatus();
+    })
+    .on("error", function(error) {
+      log(error.toString() + "(先前定存已存在)");
+      // 更新介面
+      doneTransactionStatus();
+    });
+  bank.events.PurchaseCDEvent().on("data", function(event) {
+    log(event, "PurchaseCDEvent");
+  });
+});
+
+// 當按下期滿按鍵時
+completeCDButton.on("click", async function() {
+  if (bankAddress == "") {
+    return;
+  }
+
+  // 解鎖
+  let unlock = await unlockAccount();
+  if (!unlock) {
+    return;
+  }
+
+  // 更新介面
+  waitTransactionStatus();
+  // 提款
+  bank.methods
+    .completeCD()
+    .send({
+      from: nowAccount,
+      gas: 3400000
+    })
+    .on("receipt", function(receipt) {
+      log(receipt.events.CompleteCDEvent.returnValues, "定存返還成功");
+
+      // 觸發更新帳戶資料
+      update.trigger("click");
+
+      // 更新介面
+      doneTransactionStatus();
+    })
+    .on("error", function(error) {
+      log(error.toString() + "(定存不存在)");
+      // 更新介面
+      doneTransactionStatus();
+    });
+});
+
+// 當按下提前解約按鍵時
+terminateCDButton.on("click", async function() {
+  if (bankAddress == "") {
+    return;
+  }
+  if(guard("完成期數", terminateCDPeriods.val())) {
+    return;
+  }
+
+  // 解鎖
+  let unlock = await unlockAccount();
+  if (!unlock) {
+    return;
+  }
+
+  // 更新介面
+  waitTransactionStatus();
+  // 提款
+  bank.methods
+    .terminateCD(parseInt(terminateCDPeriods.val()))
+    .send({
+      from: nowAccount,
+      gas: 3400000
+    })
+    .on("receipt", function(receipt) {
+      log(receipt.events.TerminateCDEvent.returnValues, "提前解約成功");
+
+      // 觸發更新帳戶資料
+      update.trigger("click");
+
+      // 更新介面
+      doneTransactionStatus();
+    })
+    .on("error", function(error) {
+      log(error.toString() + "(定存不存在或期數超過)");
+      // 更新介面
+      doneTransactionStatus();
+    });
+});
+
+// 載入bank合約
+function loadBank(address) {
+  if (!(address === undefined || address === null || address === "")) {
+    let bank_temp = new web3.eth.Contract(bankAbi);
+    bank_temp.options.address = address;
+
+    if (bank_temp != undefined) {
+      bankAddress = address;
+      bank.options.address = bankAddress;
+
+      contractAddress.text("合約位址:" + address);
+      log(bank, "載入合約");
+
+      update.trigger("click");
+    } else {
+      log(address, "載入失敗");
+    }
+  }
+}
+
+// 新增bank合約
+async function newBank() {
+  // 解鎖
+  let unlock = await unlockAccount();
+  if (!unlock) {
+    return;
+  }
+
+  // 更新介面
+  waitTransactionStatus();
+
+  bank
+    .deploy({
+      data: bankBytecode
+    })
+    .send({
+      from: nowAccount,
+      gas: 3400000
+    })
+    .on("receipt", function(receipt) {
+      log(receipt, "部署合約");
+
+      // 更新合約介面
+      bankAddress = receipt.contractAddress;
+      bank.options.address = bankAddress;
+      contractAddress.text("合約位址:" + receipt.contractAddress);
+      deployedContractAddressInput.val(receipt.contractAddress);
+
+      update.trigger("click");
+
+      // 更新介面
+      doneTransactionStatus();
+    });
+}
+
+function waitTransactionStatus() {
+  $("#accountStatus").html(
+    '帳戶狀態 <b style="color: blue">(等待交易驗證中...)</b>'
+  );
+}
+
+function doneTransactionStatus() {
+  $("#accountStatus").text("帳戶狀態");
+}
+
+async function unlockAccount() {
+  let password = prompt("請輸入你的密碼", "");
+  if (password == null) {
+    return false;
+  } else {
+    return web3.eth.personal
+      .unlockAccount(nowAccount, password, 60)
+      .then(function(result) {
+        alert("密碼正確" + String(result));
+        return true;
+      })
+      .catch(function(err) {
+        alert("密碼錯誤");
+        return false;
+      });
+  }
+}
